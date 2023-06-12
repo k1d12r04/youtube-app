42c6f15e6857ccf5803d7b3a17f063203bc3f927
Kadir <k1d12r04@gmail.com>
2023-06-12 20:17:45 +0300

    Prepare feed component to execute fetching data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { Sidebar, Videos } from './';
+import { fetchFromAPI } from './../utils/fetchFromAPI';
 
-const Feed = () => (
-  <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
-    <Box
-      sx={{
-        height: { sx: 'auto', md: '92vh' },
-        borderRight: '1px solid #3d3d3d',
-        px: { sx: 0, md: 2 },
-      }}
-    >
-      <Sidebar></Sidebar>
-      <Typography
-        className="copyright"
-        variant="body2"
-        sx={{ mt: 1.5, color: '#fff' }}
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
       >
-        💻 2023 k1d12r
-      </Typography>
-    </Box>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        ></Sidebar>
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          💻 2023 k1d12r
+        </Typography>
+      </Box>
 
-    <Box p={2} sx={{ overflowY: 'auto', flex: 2, height: '90vh' }}>
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: '#fff' }}>
-        New <span style={{ color: '#f31503' }}>videos</span>
-      </Typography>
-      <Videos videos={[]} />
-    </Box>
-  </Stack>
-);
+      <Box p={2} sx={{ overflowY: 'auto', flex: 2, height: '90vh' }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: '#fff' }}
+        >
+          New <span style={{ color: '#f31503' }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
