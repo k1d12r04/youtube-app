c28ab7bfe659e803def97034fd461f13455d8a07
Kadir <k1d12r04@gmail.com>
2023-06-12 16:15:41 +0300

    Add feed component and structure it

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,25 @@
-const Feed = () => {
-  return <div>Feed</div>;
-};
+import { useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+    <Box
+      sx={{
+        height: { sx: 'auto', md: '92vh' },
+        borderRight: '1px solid #3d3d3d',
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      Sidebar
+      <Typography
+        className="copyright"
+        variant="body2"
+        sx={{ mt: 1.5, color: '#fff' }}
+      >
+        💻 2023 k1d12r
+      </Typography>
+    </Box>
+  </Stack>
+);
 
 export default Feed;
