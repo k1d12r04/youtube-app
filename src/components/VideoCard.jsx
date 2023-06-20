9f0be5d3542b6540ce59fd2f2073081eebe2acd1
Kadir <k1d12r04@gmail.com>
2023-06-20 21:43:17 +0300

    Make video cards and fulfill its info

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -15,7 +15,13 @@ const VideoCard = ({ video }) => {
   const snippet = video.snippet;
 
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { md: '320px', xs: '100%' },
+        boxShadow: 'none',
+        borderRadius: 0,
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
@@ -23,6 +29,33 @@ const VideoCard = ({ video }) => {
           sx={{ width: 358, height: 180 }}
         />
       </Link>
+
+      <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle
+              sx={{
+                fontSize: 12,
+                color: 'gray',
+                ml: '10px',
+                verticalAlign: 'middle',
+              }}
+            />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
