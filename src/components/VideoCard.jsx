d3a5107109a6651d2f432d3ca7b3703b62f35d4d
Kadir <k1d12r04@gmail.com>
2023-06-20 21:16:57 +0300

    Show card media

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,5 +1,30 @@
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+import { Link } from 'react-router-dom';
+import { Typography, Card, CardContent, CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+import {
+  demoThumbnailUrl,
+  demoChannelUrl,
+  demoVideoUrl,
+  demoChannelTitle,
+  demoVideoTitle,
+} from './../utils/constants';
+
+const VideoCard = ({ video }) => {
+  const videoId = video.id.videoId;
+  const snippet = video.snippet;
+
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+    </Card>
+  );
 };
 
 export default VideoCard;
