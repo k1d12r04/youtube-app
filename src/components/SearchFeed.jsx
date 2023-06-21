e6485b8486b6e5e8abf01b84bc858db5bb238016
Kadir <k1d12r04@gmail.com>
2023-06-21 17:59:41 +0300

    Prepare api call for searching and showing results

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/material';
+
+import { Videos } from './';
+import { fetchFromAPI } from './../utils/fetchFromAPI';
+import { useParams } from 'react-router-dom';
+
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then(data =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: 'auto', flex: 2, height: '90vh' }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: '#fff' }}>
+        Search Results for:{' '}
+        <span style={{ color: '#f31503' }}>{searchTerm}</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
